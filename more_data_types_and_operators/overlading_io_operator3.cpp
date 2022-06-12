24ca106ea1aad97e6956ac9bd4eb6cc00e70fc96
harunlakodla <harunlakodla@gmail.com>
2022-06-12 04:56:43 +0300

    operator 3 açıklanması yapıldı. istream ile cin den gelen dataların girişi yapılmaktadır.

--- more_data_types_and_operators/overlading_io_operator3.cpp
+++ more_data_types_and_operators/overlading_io_operator3.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+/**
+ * @brief ThreeD sınıfına hem üzerine veri yazdırılmakta 
+ * hemde üzerinden veriler çekilebilmektedir böylece verilerin gösterilmesi sağlanmıştır...
+ * 
+ *  friend fonksiyonu ile istream ve ostream tanımlanarak private tanımlı olan
+ * değişkenler üzerine yeni veriler işlenmektedir.
+ * 
+ */
+
+
 class ThreeD
 {
 private:
@@ -33,7 +43,14 @@ std::ostream &operator<<(std::ostream &stream,ThreeD obj){
     return stream;
 }
 
-
+/**
+ * @brief bu fonksiyon sayesinde verilerimizi girdisi olarak sınıf verilerek giriş işlemi yapılmaktadr
+ * 
+ * @param stream bize direkt olarak verinin girilmesini sağlamaktadır.
+ * @param obj yazdırılmak istennen verinin sınıf türünü belirtmektedir
+ * @return std::istream& bize "cin" den gelen verinin gösterilmesini sağlamaktadır. 
+ */
+// >> ile giriş yapılacak dataların tanımlanmasını  sağlamaktadır.
 std::istream &operator>>(std::istream &stream,ThreeD &obj){
     stream>>obj.x>>obj.y>>obj.z;
 
@@ -45,8 +62,9 @@ int main(int argc, char const *argv[])
     /* code */
     ThreeD a(1,2,3),b(0,0,0);
     std::cout << a;
-    std::cin>>b;
+    std::cin>>b;// istream operatörü ile cinden gelen dataların girişini sağlamaktadır.
 
-    std::cout << b;
+    std::cout << b;// değişkenkerin üzerine yazılan veri ürü ile
+    // yazdırılma işlemleri yapılmaktadır...
     return 0;
 }
