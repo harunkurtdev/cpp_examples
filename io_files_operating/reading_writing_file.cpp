ca931ec9e625909dd91f91b44b77736ee2619cef
harunlakodla <harunlakodla@gmail.com>
2022-06-12 16:24:28 +0300

    writing commit reading writing file cpp

--- io_files_operating/reading_writing_file.cpp
+++ io_files_operating/reading_writing_file.cpp
@@ -1,12 +1,32 @@
+/**
+ * @file reading_writing_file.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <iostream>
 #include<fstream>
 
+/**
+ * @brief vbu örnekte biz direkt olarak verilen
+ * ofstream ile bir dosya oluşturdu ve üzerine yazdırma işlemleri yapmış bulunmaktayız
+ * 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 
 int main(int argc, char const *argv[])
 {
 
     std::ofstream out("file.txt");
 
+    //dosya açılmamış ise direkt olarak bu işlem içerisine gir ve yazıyı sonlandır....
     if (!out)
     {
         std::cout << "not opened file" << '\n';
@@ -14,8 +34,9 @@ int main(int argc, char const *argv[])
         /* code */
     }
 
+    //doysa üzerine yazmasını sağla
     out<<"file xx"<<std::endl;
-
+    //dosyayı kapatma işlemi
     out.close();
     
 
