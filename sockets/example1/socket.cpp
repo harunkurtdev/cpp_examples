212c6d455ebdf619e3025187a2443370193337a5
harunkurt.me <harunlakodla@gmail.com>
2022-09-11 18:02:00 +0300

    added sockets/example socketServer method for client

--- sockets/example1/socket.cpp
+++ sockets/example1/socket.cpp
@@ -1,6 +1,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <iostream>
 
 using namespace std;
@@ -14,7 +17,25 @@ int main(){
 
     server =socket(AF_INET,SOCK_STREAM,0);
 
+    serverAddr.sin_addr.s_addr=INADDR_ANY;
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_port=htons(5555);
 
+    bind(server,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+    listen(server,0);
+
+    std::cout<<"Listenning for client"<<endl;
+
+    char buffer[1024]={0};
+
+    client=accept(server,(struct sockaddr*)NULL,NULL);
+
+    read(client,buffer,sizeof(buffer)-1);
+
+    std::cout<<"client says: buffer is come"<<buffer<<endl;
+    memset(buffer,0,sizeof(buffer));
+
+    close(client);
 
     return 0;
 }
